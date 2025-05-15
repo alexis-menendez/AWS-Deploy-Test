283470dfbeabb51558da2056b3a6bd8826774d9d
Alex Menendez <alexis.246.menendez@gmail.com>
2025-05-15 15:22:44 -0500

    Add files via upload

---
+++ client/src/components/nav/NavBar.tsx
@@ -0,0 +1,20 @@
+// File: src/components/nav/NavBar.tsx
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../../assets/css/common/Nav.css';
+
+const TopNav: React.FC = () => {
+  return (
+    <nav className="dashboard-nav">
+      <div className="nav-title">🌌 Inner Orbit</div>
+      <ul className="nav-links">
+        <li><NavLink to="/dashboard" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink></li>
+        <li><NavLink to="/journal" className={({ isActive }) => isActive ? "active" : ""}>Journal</NavLink></li>
+        <li><NavLink to="/tracker" className={({ isActive }) => isActive ? "active" : ""}>Tracker</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default TopNav;
